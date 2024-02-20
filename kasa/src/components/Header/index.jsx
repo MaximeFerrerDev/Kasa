57cd8490ce86c8db3f7ba0a62e06f117cb3d1ae5
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2024-02-20 14:00:05 +0100

    Header style added

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom'
+import OrangeLogo from '../../assets/orangelogo.svg'
+import '../../styles/Header.scss'
+
 function Header() {
   return (
-    <>
-      <p> This is the header component</p>
-    </>
+    <header>
+      <img className="header__logo" src={OrangeLogo} alt="Kasa logo" />
+      <nav className="header__nav-container">
+        <Link className="header__nav-container__link" to="/">
+          Accueil
+        </Link>
+        <Link className="header__nav-container__link" to="/about">
+          A propos
+        </Link>
+      </nav>
+    </header>
   )
 }
 
