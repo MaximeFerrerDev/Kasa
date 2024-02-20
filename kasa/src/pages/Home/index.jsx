98a97faad675efe97e1be1b55ebbb5874f064532
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2024-02-20 14:41:40 +0100

    Added hero component to home page and its style

--- kasa/src/pages/Home/index.jsx
+++ kasa/src/pages/Home/index.jsx
@@ -1,15 +1,9 @@
-import { Link } from 'react-router-dom'
+import Hero from '../../components/Hero'
 
 function Home() {
   return (
     <>
-      <h1>Hello 😊</h1>
-      <p> This is the homepage</p>
-      <Link to="/accomodation/42">Cliquez ici pour voir le logement 42</Link>
-      <br />
-      <Link to="/about">A propos</Link>
-      <br />
-      <Link to="/error">Error 404</Link>
+      <Hero />
     </>
   )
 }
