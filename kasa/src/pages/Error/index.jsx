d84556998d1ef540482c33230b73b010741f3228
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2024-02-21 10:59:46 +0100

    Error page added

--- kasa/src/pages/Error/index.jsx
+++ kasa/src/pages/Error/index.jsx
@@ -1,9 +1,17 @@
+import '../../styles/Error.scss'
+import { Link } from 'react-router-dom'
+
 function Error() {
   return (
-    <>
-      <h1>Error 404</h1>
-      <p> Sorry 😂</p>
-    </>
+    <div className="error-container">
+      <h1 className="error-title">404</h1>
+      <p className="error-description">
+        Oups! La page que vous demandez n'existe pas
+      </p>
+      <Link to="/" className="error-linkback">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
   )
 }
 
