dc99d567999cad63bce52e471d2191d7268cb0b2
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2024-02-21 10:27:57 +0100

    Template accomodation cards added to home page and styled

---
+++ kasa/src/components/Accomodation Card/index.jsx
@@ -0,0 +1,11 @@
+import '../../styles/AccomodationCard.scss'
+
+function AccomodationCard() {
+  return (
+    <div className="accomodation-card">
+      <h2 className="accomodation-card__title">Titre de la location</h2>
+    </div>
+  )
+}
+
+export default AccomodationCard
--- kasa/src/pages/Home/index.jsx
+++ kasa/src/pages/Home/index.jsx
@@ -1,9 +1,21 @@
+import AccomodationCard from '../../components/Accomodation Card'
 import Hero from '../../components/Hero'
+import '../../styles/Home.scss'
+
+// Need to find a solution to get a pretty display on all desktop sizes
 
 function Home() {
   return (
     <>
       <Hero />
+      <div className="accomodation-cards-container">
+        <AccomodationCard />
+        <AccomodationCard />
+        <AccomodationCard />
+        <AccomodationCard />
+        <AccomodationCard />
+        <AccomodationCard />
+      </div>
     </>
   )
 }
