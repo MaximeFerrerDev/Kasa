25579ab1f106fea96688c2b014970c6941705805
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2024-02-24 11:37:43 +0100

    Adding heading and content text props to collapse bar component

--- kasa/src/components/CollapseBar/index.jsx
+++ kasa/src/components/CollapseBar/index.jsx
@@ -2,8 +2,12 @@ import CollapseIcon from '../../assets/collapseicon.svg'
 import '../../styles/components/CollapseBar.scss'
 import { useState } from 'react'
 
-function CollapseBar() {
+function CollapseBar(props) {
+  const heading = props.heading
+  const textContent = props.textContent
+
   const [isActive, setActive] = useState(false)
+
   const handleClick = (event) => {
     // Setting the max-height manually from the scroll height to fit every text
     let content = event.target.nextElementSibling
@@ -22,7 +26,7 @@ function CollapseBar() {
         className={`collapse-bar ${isActive ? 'active' : ''}`}
         onClick={(event) => handleClick(event)}
       >
-        <p className="collapse-bar__heading">Heading</p>
+        <p className="collapse-bar__heading">{heading}</p>
         <img
           className="collapse-bar__icon"
           src={CollapseIcon}
@@ -30,39 +34,7 @@ function CollapseBar() {
         />
       </button>
       <div className="collapse-content">
-        <p className="collapse-content__text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-          risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
-          ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
-          massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci
-          nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl
-          sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae,
-          consequat in, pretium a, enim. Pellentesque congue. Ut in risus
-          volutpat libero pharetra tempor. Cras vestibulum bibendum augue.
-          Praesent egestas leo in pede. Praesent blandit odio eu enim.
-          Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum
-          primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-          Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum.
-          Maecenas adipiscing ante non diam sodales hendrerit. Ut velit mauris,
-          egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa
-          suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus
-          aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae
-          ligula. Pellentesque rhoncus nunc et augue. Integer id felis.
-          Curabitur aliquet pellentesque diam. Integer quis metus vitae elit
-          lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing
-          elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien.
-          Integer tortor tellus, aliquam faucibus, convallis id, congue eu,
-          quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non
-          elementum posuere, metus purus iaculis lectus, et tristique ligula
-          justo vitae magna. Aliquam convallis sollicitudin purus. Praesent
-          aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac
-          euismod nibh nisl eu lectus. Fusce vulputate sem at sapien. Vivamus
-          leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo placerat
-          imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum
-          augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel
-          rhoncus faucibus, felis magna fermentum augue, et ultricies lacus
-          lorem varius purus. Curabitur eu amet.
-        </p>
+        <p className="collapse-content__text">{textContent}</p>
       </div>
     </div>
   )
--- kasa/src/pages/About/index.jsx
+++ kasa/src/pages/About/index.jsx
@@ -2,13 +2,19 @@ import CollapseBar from '../../components/CollapseBar'
 import '../../styles/pages/About.scss'
 
 function About() {
+  const shortText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.'
+
+  const longText =
+    'Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.'
+
   return (
     <main className="about-main">
       <div className="about-hero"></div>
-      <CollapseBar />
-      <CollapseBar />
-      <CollapseBar />
-      <CollapseBar />
+      <CollapseBar heading="Fiabilité" textContent={shortText} />
+      <CollapseBar heading="Respect" textContent={longText} />
+      <CollapseBar heading="Service" textContent={shortText} />
+      <CollapseBar heading="Sécurité" textContent={longText} />
     </main>
   )
 }
